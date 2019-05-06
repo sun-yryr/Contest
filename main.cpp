30a8d4d65a1b69c6e23adb824b982535018908b5
sun-yryr <taittide@gmail.com>
2019-05-06 19:09:17 +0900

    a

--- main.cpp
+++ main.cpp
@@ -10,29 +10,12 @@ using namespace std;
 #define ll long long
 ll MOD = 10e9 + 7;
 
-int main() {
-    int n = 5;
-    string s = "##.#.";
-  	//cin >> n;
-    //cin >> s;
-    int black[n],white[n];
-    REP(i, n) {
-        if(i==0) black[i] = (s[i]=='#'? 1: 0);
-        else if(s[i] == '#') black[i] = black[i-1] + 1;
-        else black[i] = black[i-1];
-    }
-    for(int i=n-1; i>=0; i--) {
-        if(i==n-1) white[i] = (s[i]=='.'? 1: 0);
-        else if(s[i] == '.') white[i] = white[i+1] + 1;
-        else white[i] = white[i+1];
-    }
-    int min_change = min(n - black[n-1], black[n-1]);
-    int tmp_b, tmp_w;
-    REP(i, n) {
-        tmp_b = black[i];
-        tmp_w = white[i+1];
-        min_change = min(min_change, tmp_b+tmp_w);
-    }
-    OUT(min_change);
+int main(int argc, char *argv[]) {
+    string line, reverse_line;
+    cin >> line;
+    reverse_line = line;
+    reverse_copy(line.begin(), line.end(), reverse_line.begin());
+    OUT(reverse_line);
+    OUT(line);
     return 0;
 }
