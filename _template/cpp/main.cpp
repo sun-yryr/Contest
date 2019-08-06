d64564aa591cba8cbeace2d15ef7478bd8bb2d14
sun-yryr <taittide@gmail.com>
2019-08-06 12:59:09 +0900

    123

--- _template/cpp/main.cpp
+++ _template/cpp/main.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 #define REP(i, n) for(int (i)=0; (i)<(n); (i)++)
+#define ALL(x) (x).begin(), (x).end()
 #define OUT(a) cout << (a) << "\n"
-#define ll long long
-#define uint unsigned int
-#define ushort unsigned short
-#define ulong unsigned long
 
-ll MOD = 10e9 + 7;
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ll MOD = 10e9 + 7;
 
 int main() {
     int n;
--- abc123/d.cpp
+++ abc123/d.cpp
@@ -1,30 +1,58 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int (i)=0; (i)<(n); (i)++)
-#define OUT(a) cout << (a) << "\n";
-#define ll long long
 
-const ll MOD = 1e9 + 7;
+#define REP(i, n) for(int (i)=0; (i)<(n); (i)++)
+#define ALL(x) (x).begin(), (x).end()
+#define OUT(a) cout << (a) << "\n"
+
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ll MOD = 10e9 + 7;
+int lists[1000][1000];
+
+struct cake {
+    ll cost;
+    int i;
+    int j;
+    bool operator<(const cake& key) const {
+        return this->cost < key.cost;
+    }
+};
+cake make_cake(ll c, int ii, int jj) {
+    cake a;
+    a.cost = c; a.i = ii, a.j = jj;
+    return a;
+}
 
 int main() {
     int x,y,z,k;
     cin >> x >> y >> z >> k;
-    ll a[1000], b[1000], c[1000], ans[k]={0};
-    REP(i, x) scanf("%lld ", &a[i]);
-    REP(i, y) scanf("%lld ", &b[i]);
-    REP(i, z) scanf("%lld ", &c[i]);
-    ll tmp;
-    REP(i, x) REP(j,y) REP(m, z) {
-        tmp = a[i] + b[j] + c[m];
-        ans[k-1] = max(ans[k-1], tmp);
-        sort(ans, ans+k, greater<ll>());
+    vector<ll> a(x), b(y), c(z);
+    REP(i, x) scanf("%lld", &a[i]);
+    REP(i, y) scanf("%lld", &b[i]);
+    REP(i, z) scanf("%lld", &c[i]);
+    sort(ALL(a));
+    sort(ALL(b));
+    sort(ALL(c), greater<ll>());
+
+    priority_queue<cake> q;
+    REP(i, x) REP(j, y) {
+        lists[i][j] = 1;
+        q.push(make_cake(a[i]+b[j]+c[0], i, j));
     }
-    REP(i,k) printf("%lld\n", ans[i]);
+    REP(p, k) {
+        cake tmp = q.top();
+        q.pop();
+        int i = tmp.i;
+        int j = tmp.j;
+        printf("%lld\n", tmp.cost);
+        
+        if (lists[i][j] < z) {
+            q.push(make_cake(a[i]+b[j]+c[lists[i][j]], i, j));
+            lists[i][j]++;
+        }
+    }
     return 0;
 }
